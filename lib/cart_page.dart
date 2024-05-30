741ebcb545ca1e629fa8206bb41544764af22c3a
AmndanDwii <amndan01@gmail.com>
2024-05-30 10:05:18 +0700

    praktikum ke3

---
+++ lib/cart_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'cart_model.dart';
+import 'product_page.dart';
+
+class CartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<CartModel>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart Page'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.cart.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(cart.cart[index].name),
+                  subtitle: Text('\$${cart.cart[index].price} x ${cart.cart[index].quantity}'),
+                  trailing: Text('Total: \$${cart.cart[index].price * cart.cart[index].quantity}'),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Total Price: \$${cart.getTotalPrice().toStringAsFixed(2)}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/product_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'cart_model.dart';
+
+class ProductPage extends StatefulWidget {
+  @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  final List<Product> products = [
+    Product(name: "Product 1", price: 10),
+    Product(name: "Product 2", price: 20),
+    Product(name: "Product 3", price: 30),
+    Product(name: "Product 4", price: 40),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Page'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ProductListItem(product: products[index]);
+        },
+      ),
+    );
+  }
+}
+
+class Product {
+  final String name;
+  final int price;
+  int quantity;
+
+  Product({required this.name, required this.price, this.quantity = 0});
+}
+
+class ProductListItem extends StatelessWidget {
+  final Product product;
+
+  ProductListItem({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<CartModel>(context);
+
+    return ListTile(
+      title: Text(product.name),
+      subtitle: Text('\$${product.price}'),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            icon: Icon(Icons.remove),
+            onPressed: () {
+              cart.remove(product);
+            },
+          ),
+          Text(product.quantity.toString()),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              cart.addToCart(product);
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              cart.addToCart(product);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('${product.name} added to cart!'),
+                  duration: Duration(seconds: 2),
+                ),
+              );
+            },
+            child: Text('Add to Cart'),
+          ),
+        ],
+      ),
+    );
+  }
+}
